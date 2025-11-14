616b725414f04a6dac5916521667a201d83ba04f
aseican <emreecansever@gmail.com>
2025-11-14 12:23:10 +0300

    Son ve kesin cozum: main.jsx App export hatasi duzeltildi

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
-import '@/index.css';
-import { Toaster } from '@/components/ui/toaster';
-import { AuthProvider } from '@/contexts/SupabaseAuthContext';
+import { App } from '@/App'; // <-- SON VE KESİN DÜZELTME: { App } eklendi
+import './index.css';
+import { AuthProvider } from './contexts/SupabaseAuthContext';
+import { Toaster } from './components/ui/toaster'; // Toast bildirimleri için gerekli
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <React.StrictMode>
     <AuthProvider>
       <App />
-      <Toaster />
+      <Toaster /> {/* Bildirimleri göstermek için ekledik */}
     </AuthProvider>
-  </>
+  </React.StrictMode>
 );
